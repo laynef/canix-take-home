a48076380966ddee65d40ecc224d94a8bb12cfec
Layne Faler <laynef@DESKTOP-O9JEHLF.localdomain>
2023-06-17 12:42:49 -0700

    Added typescript and react

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,5 +1,6 @@
-const { webpackConfig } = require('shakapacker')
+const { webpackConfig, merge } = require("shakapacker");
+const ForkTSCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 
-// See the shakacode/shakapacker README and docs directory for advice on customizing your webpackConfig.
-
-module.exports = webpackConfig
+module.exports = merge(webpackConfig, {
+  plugins: [new ForkTSCheckerWebpackPlugin()],
+});
