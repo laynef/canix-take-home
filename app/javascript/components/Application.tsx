c376ca76f8d871bbeb555822e8f3111643a2cb4c
Layne Faler <laynef@DESKTOP-O9JEHLF.localdomain>
2023-06-18 15:33:06 -0700

    Reranmed HelloWorld to Application

---
+++ app/javascript/components/Application.tsx
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+type ApplicationProps = {};
+
+const Application: React.FC<ApplicationProps> = () => {
+  return (
+    <h1 className="text-center w-100 font-weight-light">
+      Hello World!
+    </h1>
+  );
+};
+
+export default Application
