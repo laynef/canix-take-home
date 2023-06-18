f622b8dde9eeb1153cba011f9d0171ba02f96a57
Layne Faler <laynef@DESKTOP-O9JEHLF.localdomain>
2023-06-18 15:23:46 -0700

    Rendering Hellow World component with server side and bootstrap

--- app/javascript/components/HelloWorld.tsx
+++ app/javascript/components/HelloWorld.tsx
@@ -6,9 +6,9 @@ type HelloWorldProps = {
 
 const HelloWorld: React.FC<HelloWorldProps> = (props) => {
   return (
-    <>
+    <h1 className="text-center w-100 font-weight-light">
       Hello, {props.greeting}!
-    </>
+    </h1>
   );
 };
 
